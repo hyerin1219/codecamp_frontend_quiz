e9dea08dd71ffddaa8313f70196cbd1e679c5876
Your Name <you@example.com>
2024-01-15 11:13:10 +0900

    quiz07

--- quiz/pages/07/products/01/index.js
+++ quiz/pages/07/products/01/index.js
@@ -1,8 +1,8 @@
-import { useQuery, gql } from "@apollo/client"
+import { useQuery, gql, useMutation } from "@apollo/client"
 
 const FETCH_PRODUCTS = gql`
-    mutation fetchProducts($Page: Int){
-        fetchProducts(page: $page) {
+    query {
+        fetchProducts{
             _id
             seller
             name
@@ -11,14 +11,52 @@ const FETCH_PRODUCTS = gql`
         }
 }
 `
+
+const DELETE_PRODUCT = gql`
+    mutation deleteProduct($productId: ID) {
+        deleteProduct(productId: $productId) {
+            _id
+            number
+            message
+        }
+}
+`
+
+
 export default function createProductsPage() {
 
-    const [fetchProducts] = useQuery(FETCH_PRODUCTS)
+    const { data } = useQuery(FETCH_PRODUCTS)
 
-    
+    // console.log(data?.fetchProducts)
+
+    const [deleteProduct] = useMutation(DELETE_PRODUCT)
+
+    const onclickDelete = (event) => {
+        deleteProduct({
+            variables: {productId: event.target._id},
+            refetchQueries: [{query: FETCH_PRODUCTS}]
+        })
+
+    }
+
+
 
     return (
 
+        <div>
+            {data?.fetchProducts.map(el => (
+                <div key={el._id}>
+                    <input type="checkBox"/>
+                    <span>{el._id}</span>
+                    <span>{el.seller}</span>,
+                    <span>{el.name}</span>,
+                    <span>{el.detail} </span>,
+                    <span>{el.price}</span>
+                    <button id={el._id} onClick={onclickDelete}>삭제</button>
+                    {console.log(el._id)}
+                </div>
+            ))}
+        </div>
         
 
     )
