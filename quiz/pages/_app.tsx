4f50901185c588f784b994347f8d2d953a7f9e51
Your Name <you@example.com>
2024-01-24 11:14:45 +0900

    typescript 다운, 크래픽큐엘 코드젠 설치

--- quiz/pages/_app.tsx
+++ quiz/pages/_app.tsx
@@ -1,8 +1,9 @@
 // import '@/styles/globals.css'
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { AppProps } from 'next/app'
 
-export default function App({ Component}) {
+export default function App({ Component}:AppProps) {
 
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
