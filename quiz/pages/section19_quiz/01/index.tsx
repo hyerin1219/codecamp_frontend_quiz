af0665b5bbd3e180b5d2eeddb2412d012dd8891d
Your Name <you@example.com>
2024-02-16 11:10:04 +0900

    quiz 완료

--- quiz/pages/section19_quiz/01/index.tsx
+++ quiz/pages/section19_quiz/01/index.tsx
@@ -1,5 +1,7 @@
 import { gql, useQuery } from "@apollo/client"
 import styled from "@emotion/styled"
+import InfiniteScroll from "react-infinite-scroller"
+import type { IQuery, IQueryFetchBoardsArgs } from "../../../src/commons/types/generated/types"
 
 const FETCH_BOARDS  = gql`
         query fetchBoards($page: Int) {
@@ -16,23 +18,55 @@ export default function InfinitePage():JSX.Element {
 
     const DivBox = styled.div`
         height: 500px;
-        background-color: tan;
+        overflow: auto;
     `
 
     const DivFlex =styled.div`
         display: flex;
     `
 
-    const { data, fetchMore } = useQuery(FETCH_BOARDS)
+    const DivContent = styled.div`
+        margin-right: 20px;
+    `
+
+    const { data, fetchMore } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(FETCH_BOARDS)
+
+    const onLoadMore = ():void => {
+        if(data === undefined) return
+
+        void fetchMore({
+            variables: {page: Math.ceil(data?.fetchBoards.length / 10 ?? 10) +1 },
+            updateQuery: (prev, {fetchMoreResult}) => {
+                if(fetchMoreResult.fetchBoards === undefined) {
+                    return {
+                        fetchBoards: [...prev.fetchBoards]
+                    }
+                }
+                return {
+                    fetchBoards: [...prev.fetchBoards, ...fetchMoreResult.fetchBoards]
+                }
+            }
+        })
+    }
 
     return (
         <>
             <DivBox>
-                <DivFlex>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </DivFlex>
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={onLoadMore}
+                    hasMore={true}
+                    useWindow={false}
+                >
+                    {data?.fetchBoards.map( (el) => (
+                        <DivFlex key={el._id}>
+                            <DivContent>{el.writer}</DivContent>
+                            <DivContent>{el.title}</DivContent>
+                            <DivContent>{el.contents}</DivContent>
+                        </DivFlex>
+                    ))}
+                    
+                </InfiniteScroll>
             </DivBox>
         </>
     )
