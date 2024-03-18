10cc917112bcfaa33de74bceac13fb80dd3f4481
Your Name <you@example.com>
2024-03-18 17:40:41 +0900

    소주제 1번까지 quiz 완료

---
+++ quiz/src/commons/stores/index.tsx
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+
+export const accessTokenState = atom({
+    key: "accessTokenState",
+    default: ""
+})
--- quiz/src/components/commons/apollo/index.tsx
+++ quiz/src/components/commons/apollo/index.tsx
@@ -1,5 +1,8 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink } from '@apollo/client'
 import { createUploadLink } from 'apollo-upload-client'
+import { useRecoilState } from 'recoil'
+import { accessTokenState } from '../../../commons/stores'
+import { useEffect } from 'react'
 
 interface IApolloSettingProps {
     children: JSX.Element
@@ -7,8 +10,18 @@ interface IApolloSettingProps {
 
 export default function ApolloSetting(props: IApolloSettingProps):JSX.Element {
 
+    const [accessToken, setAccessToken] = useRecoilState(accessTokenState)
+
+    useEffect(() => {
+        const result = localStorage.getItem("accessToken")
+        setAccessToken(result ?? "")
+    }, [])
+
     const uploadLink = createUploadLink({
         uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
     })
 
     const client = new ApolloClient({
